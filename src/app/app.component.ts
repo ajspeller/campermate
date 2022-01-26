bba8f59fb152fdcb4ce5c49335aa948406c66c6e
AJ Speller <ajspeller@gmail.com>
2022-01-25 22:01:59 -0500

    Add splash-screen, status-bar, storage-angular and sqlite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SplashScreen } from '@capacitor/splash-screen';
+import { StatusBar } from '@capacitor/status-bar';
+
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,15 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private storage: Storage) {
+    this.initializeApp();
+  }
+
+  async initializeApp() {
+    await this.storage.create();
+    SplashScreen.hide().catch((err) => console.warn(err));
+    StatusBar.setBackgroundColor({ color: '#54a0ff' }).catch((err) =>
+      console.warn(err)
+    );
+  }
 }
